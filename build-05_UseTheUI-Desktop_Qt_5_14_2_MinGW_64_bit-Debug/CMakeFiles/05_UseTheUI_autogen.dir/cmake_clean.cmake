3e652013928a3ee3c3580e93022f4a9182cabcaa
ScorpioMax <2252030510@qq.com>
2020-08-19 16:10:55 +0800

    delete some documents

--- build-05_UseTheUI-Desktop_Qt_5_14_2_MinGW_64_bit-Debug/CMakeFiles/05_UseTheUI_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "05_UseTheUI_autogen/mocs_compilation.cpp"
-  "CMakeFiles/05_UseTheUI_autogen"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/05_UseTheUI_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
